c3b2cf7f2f11685be51810dd49dbfedc3c74c57e
Hossein <123118261+HosseinVT@users.noreply.github.com>
2025-04-25 20:08:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html, Input, Output, State, dash_table
 from flask import Flask
+import pyswmm
 from pyswmm import Simulation, Nodes , Links
 from swmm.toolkit.shared_enum import SubcatchAttribute, NodeAttribute, LinkAttribute
 import plotly.express as px
